3b83892615f5db975d8e75e710b68f0b75780b56
Oliver Carranza <olivercd15@gmail.com>
2025-08-25 16:36:52 -0400

    fix: Asign relative paths for Jenkinsfile.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ const PrivateRoute = ({ children }) => {
 function App() {
   return (
     <AuthProvider>
-      <BrowserRouter basename="/">
+      <BrowserRouter basename=".">
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route 
